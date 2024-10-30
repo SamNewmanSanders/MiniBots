d9c0ef55b88c66fbf8891d6e0799870c8a3a5333
Bogdan Isaev <57876472+bogdisae@users.noreply.github.com>
2024-10-30 13:48:41 +0000

    sensor

--- sketch_oct23b/sketch_oct23b.ino
+++ sketch_oct23b/sketch_oct23b.ino
@@ -1,9 +1,48 @@
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+long duration, distance, prevdistance, prev2distance;
+
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  //Update previous readings to from an average
+  prevdistance = distance;
+  prev2distance = prevdistance;
+ 
+  // Convert the time into a distance
+  distance = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  
+  long normalised_distance = (distance + prevdistance + prev2distance)/3;
+
+  //Serial.print("Actual:");
+  //Serial.print(distance);
+  //Serial.print("cm\n");
+  Serial.print("Normalized:");
+  Serial.print(normalised_distance);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
 
 }
